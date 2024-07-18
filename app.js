c2fa6412d341de1d9e2d19b43c56f1c4a62cb1a6
Sajal <23f2002548@ds.study.iitm.ac.in>
2024-07-18 13:28:47 +0530

    mongo connect

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import AppError from "./utils/AppError";
+import productRouter from "./routers/productRouter";
+import userRouter from "./routers/userRouter";
+import reviewRouter from "./routers/reviewRouter";
+import orderRouter from "./routers/orderRouter";
+import globalErrorHandler from "./controllers/errorController";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
+app.use(cookieParser());
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     status: "ok",
   });
 });
+
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
+
+app.use("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 export default app;
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-import app from "./app.js";
-import dotenv from "dotenv";
+import { config } from "dotenv";
+import { connect } from "mongoose";
+import { listen } from "./app";
 
-dotenv.config({ path: "./.env.local" });
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled Exception!");
+  process.exit(1);
+});
+
+config({ path: "./.env.local" });
 
 const port = process.env.PORT;
 
-const server = app.listen(port, () => {
+const dbUrl = process.env.DB_URI.replace("<password>", process.env.DB_PASS);
+
+connect(dbUrl).then(() => {
+  console.log("Database is running");
+});
+
+const server = listen(port, () => {
   console.log("Server is running on the port ", port);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled Rejection!");
+  server.close(() => {
+    process.exit(1);
+  });
 });
