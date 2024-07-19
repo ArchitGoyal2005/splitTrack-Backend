e1e122bf8180b10d188d4d1b5beacf2fb03f4349
Sajal <23f2002548@ds.study.iitm.ac.in>
2024-07-19 10:26:00 +0530

    fixes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,14 @@
-const globalErrorHandler = (err, req, res, next) => {
+import AppError from "../utils/AppError";
+
+module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+  console.log(err);
 
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
 };
-
-export default globalErrorHandler;
